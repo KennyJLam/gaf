82f2c0eedbcfb19cca52cb321c1e87f3265be574
John Newcombe <john.newcombe@me.com>
2016-03-30 20:47:34 +0100

    Added .Net 4.5 version

---
+++ GAF.45/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("A simple to use GA framework for .Net")]
+[assembly: AssemblyProduct("GAF (.Net 4.5)")]
