4f4f932b9efdf459fc87f334d0732c4b1dc4033b
John Newcombe <john.newcombe@me.com>
2016-03-30 10:55:29 +0100

    Updated the assemblyinfo.cs.

--- GAF.Pcl/Properties/AssemblyInfo.cs
+++ GAF.Pcl/Properties/AssemblyInfo.cs
@@ -4,22 +4,23 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Genetic Algorithm Framework for .Net")]
+//[assembly: AssemblyTitle("Genetic Algorithm Framework for .Net")]
 [assembly: AssemblyDescription("A simple to use GA framework for .Net (4.5 pcl)")]
-[assembly: AssemblyConfiguration("17th March 2016")]
-[assembly: AssemblyCompany("AI Frameworks")]
-[assembly: AssemblyProduct("Genetic Algorithm Framework for .Net")]
-[assembly: AssemblyCopyright("Copyright © John Newcombe 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("GAF (.Net 4.5 PCL)")]
+//[assembly: AssemblyConfiguration("17th March 2016")]
+//[assembly: AssemblyCompany("AI Frameworks")]
+//[assembly: AssemblyProduct("Genetic Algorithm Framework for .Net")]
+//[assembly: AssemblyCopyright("Copyright © John Newcombe 2015")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
+//[assembly: AssemblyVersion("2.2.1")]
+//[assembly: AssemblyFileVersion("2.2.1")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
@@ -28,7 +29,7 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyKeyFile("")]
 
 //used to give unit test access
-[assembly: InternalsVisibleTo("GAF.UnitTests")]
-[assembly: InternalsVisibleTo("GAF.Lab")]
-[assembly: InternalsVisibleTo("GAF.NUnit")]
+//[assembly: InternalsVisibleTo("GAF.UnitTests")]
+//[assembly: InternalsVisibleTo("GAF.Lab")]
+//[assembly: InternalsVisibleTo("GAF.NUnit")]
 
