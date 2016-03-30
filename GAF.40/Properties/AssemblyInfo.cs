50f69c8e934e51b47f59cd698649fe4b623e0808
John Newcombe <john.newcombe@me.com>
2016-03-30 20:50:50 +0100

    Tidy References and AssemblyInfo.

--- GAF.40/Properties/AssemblyInfo.cs
+++ GAF.40/Properties/AssemblyInfo.cs
@@ -1,45 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-//[assembly: AssemblyTitle("Genetic Algorithm Framework for .Net")]
-[assembly: AssemblyDescription("A simple to use GA framework for .Net (4.0)")]
+[assembly: AssemblyDescription("A simple to use GA framework for .Net")]
 [assembly: AssemblyProduct("GAF (.Net 4.0)")]
-
-//[assembly: AssemblyConfiguration("17th March 2016")]
-//[assembly: AssemblyCompany("AI Frameworks")]
-//[assembly: AssemblyProduct("Genetic Algorithm Framework for .Net")]
-//[assembly: AssemblyCopyright("Copyright © John Newcombe 2015")]
-//[assembly: AssemblyTrademark("")]
-//[assembly: AssemblyCulture("")]
-
-//used to give unit test access
-//[assembly: InternalsVisibleTo("GAF.UnitTests")]
-//[assembly: InternalsVisibleTo("GAF.Lab")]
-//[assembly: InternalsVisibleTo("GAF.NUnit")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-//[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("c488de07-8a6f-4a10-b6b3-05a00d866c8b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-//[assembly: AssemblyVersion("2.2.1")]
-//[assembly: AssemblyFileVersion("2.2.1")]
-
 
--- GAF.Pcl/Properties/AssemblyInfo.cs
+++ GAF.Pcl/Properties/AssemblyInfo.cs
@@ -1,35 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-//[assembly: AssemblyTitle("Genetic Algorithm Framework for .Net")]
 [assembly: AssemblyDescription("A simple to use GA framework for .Net (4.5 pcl)")]
 [assembly: AssemblyProduct("GAF (.Net 4.5 PCL)")]
-//[assembly: AssemblyConfiguration("17th March 2016")]
-//[assembly: AssemblyCompany("AI Frameworks")]
-//[assembly: AssemblyProduct("Genetic Algorithm Framework for .Net")]
-//[assembly: AssemblyCopyright("Copyright © John Newcombe 2015")]
-//[assembly: AssemblyTrademark("")]
-//[assembly: AssemblyCulture("")]
-
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-//[assembly: AssemblyVersion("2.2.1")]
-//[assembly: AssemblyFileVersion("2.2.1")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
-//used to give unit test access
-//[assembly: InternalsVisibleTo("GAF.UnitTests")]
-//[assembly: InternalsVisibleTo("GAF.Lab")]
-//[assembly: InternalsVisibleTo("GAF.NUnit")]
-
