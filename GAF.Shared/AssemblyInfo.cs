3f493b3c63754fea1e6820f531aa37bc176d77d2
John Newcombe <john.newcombe@me.com>
2016-05-15 07:34:42 +0100

    Updated the version to reflec the nex build.

--- GAF.Shared/AssemblyInfo.cs
+++ GAF.Shared/AssemblyInfo.cs
@@ -12,13 +12,17 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.2.2.*")]
-[assembly: AssemblyFileVersion("2.2.2")]
+// Major: Large refactoring of re-writes.
+// Minor: Standard release via NuGet (post v2.2.2, pre v2.2.2 was a little adhoc, sorry!)
+// Build: Changes published to Git
+// Build: 
+
+[assembly: AssemblyVersion("2.2.3.*")]
+[assembly: AssemblyFileVersion("2.2.3")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
