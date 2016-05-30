481dfd9aecbc3f0a98f66f30bc51b056432c85c9
John Newcombe <john.newcombe@me.com>
2016-05-30 10:38:42 +0100

    Updated comments.

--- GAF.Shared/IConsumerFunctions.cs
+++ GAF.Shared/IConsumerFunctions.cs
@@ -9,19 +9,20 @@ namespace GAF
 	{
 
 		/// <summary>
-		/// This interface is provided to support external programs such as the GAF.Lab GUI application.
+		/// This method should implement the GA's evaluation function.
 		/// </summary>
 		/// <returns>The fitness.</returns>
 		/// <param name="chromosome">Chromosome.</param>
 		double EvaluateFitness(Chromosome chromosome);
 
 		/// <summary>
-		/// This interface is provided to support external programs such as the GAF.Lab GUI application.
+		/// This method should implement the GA's terminate function.
 		/// </summary>/// <returns><c>true</c>, if algorithm was terminated, <c>false</c> otherwise.</returns>
 		/// <param name="population">Population.</param>
 		/// <param name="currentGeneration">Current generation.</param>
 		/// <param name="currentEvaluation">Current evaluation.</param>
 		bool TerminateAlgorithm(Population population, int currentGeneration, long currentEvaluation);
+
 
 	}
 }
