eb8f77594a58b20870da9c6ea986a9f4296a77da
John Newcombe <john.newcombe@me.com>
2016-08-13 22:18:34 +0100

    GAF Evaluation Server initial import.

---
+++ GAF.EvaluationServer/Parameters.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using GAF.Consul;
+
+namespace GAF.EvaluationServer
+{
+	public class Parameters
+	{
+		private const int defaultServerPort = 11000;
+		private const int defaultConsulPort = 8500;
+
+		public Parameters (String[] args)
+		{
+			if (args != null) {
+
+				var paramPort = args.SingleOrDefault (arg => arg.StartsWith ("-port:", StringComparison.InvariantCultureIgnoreCase));
+				var paramIP = args.SingleOrDefault (arg => arg.StartsWith ("-ip:", StringComparison.InvariantCultureIgnoreCase));
+				var paramFitnessAssembly = args.SingleOrDefault (arg => arg.StartsWith ("-f:", StringComparison.InvariantCultureIgnoreCase));
+				var paramConsul = args.SingleOrDefault (arg => arg.StartsWith ("-consul:", StringComparison.InvariantCultureIgnoreCase));
+
+				//Port
+				if (!string.IsNullOrEmpty (paramPort)) {
+					paramPort = paramPort.Replace ("-port:", "");
+					var newPort = 0;
+					if (int.TryParse (paramPort, out newPort)) {
+						this.Port = newPort;
+					}
+				} else {
+					this.Port = defaultServerPort;
+				}
+
+				//IP Address
+				if (!string.IsNullOrEmpty (paramIP)) {
+
+					paramIP = paramIP.Replace ("-ip:", "");
+					IPAddress addr;
+					if (IPAddress.TryParse (paramIP, out addr)) {
+						this.IPAddress = addr;
+					}
+
+				} else {
+					//nothing specified so use local address
+					IPHostEntry ipHostInfo = Dns.GetHostEntry (Dns.GetHostName ());
+					this.IPAddress = ipHostInfo.AddressList [0];
+				}
+
+				//Consumer Functions
+				if (!string.IsNullOrEmpty (paramFitnessAssembly)) {
+					this.FitnessAssemblyName = paramFitnessAssembly.Replace ("-f:", "");
+				}
+
+				//Consul Node Address
+				if (!string.IsNullOrEmpty (paramConsul)) {
+					paramConsul = paramConsul.Replace ("-consul:", "");
+					this.ConsulNodeEndPoint = CreateEndpoint (paramConsul);
+				} else {
+					//nothing specified so use local address
+					IPHostEntry ipHostInfo = Dns.GetHostEntry (Dns.GetHostName ());
+					this.ConsulNodeEndPoint = new IPEndPoint(ipHostInfo.AddressList [0], defaultConsulPort);
+				}
+
+			}
+
+		}
+
+		public int Port { get; set; }
+
+		public IPAddress IPAddress { get; set; }
+
+		public string FitnessAssemblyName { set; get;}
+
+		public IPEndPoint ConsulNodeEndPoint { set; get; }
+
+
+		#region Helper Methods
+
+		private IPEndPoint CreateEndpoint (string endpointAddress)
+		{
+			IPEndPoint ipEndPoint = null;
+
+			if (endpointAddress.Contains (":")) {
+
+				var epSegments = endpointAddress.Split (":".ToCharArray ());
+
+				IPAddress addr = null;
+				int port = 0;
+
+				if (IPAddress.TryParse (epSegments [0], out addr) &&
+					int.TryParse (epSegments [1], out port)) {
+
+					ipEndPoint = new IPEndPoint (addr, port);
+
+				}
+			}
+
+			return ipEndPoint;
+		}
+
+		#endregion
+	}
+}
+
---
+++ GAF.EvaluationServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using GAF.Consul;
+using GAF.ConsumerFunctions.TravellingSalesman;
+using GAF.Network;
+
+namespace GAF.EvaluationServer
+{
+	public class Program
+	{
+		public static int Main (String [] args)
+		{
+			try {
+
+				//retrieves the parameters from the commant line.
+				var settings = new Parameters (args);
+
+				//register service with consul
+				//if (settings.ConsulNodeEndPoint != null) {
+
+				//var endPoint = ConsulClient.CreateEndpoint ("192.168.1.90:8500");
+				//var consulClient = new ConsulClient ("192.168.1.91", 8500);
+				var consulClient = new ConsulClient (settings.ConsulNodeEndPoint);
+
+
+				Console.WriteLine ("Registering GAF Evaluation Server with Consul Node at address {0}.",
+								   consulClient.NodeEndPoint.ToString ());
+				try {
+					//consulClient.RegisterService (serviceDefinition);
+					var serviceId = string.Format ("GAF-Server:{0}:{1}", settings.IPAddress, settings.Port);
+					var endPoint = ConsulClient.CreateEndpoint (string.Format ("{0}:{1}", settings.IPAddress, settings.Port));
+					var testEndPoint = ConsulClient.CreateEndpoint (string.Format ("{0}:{1}", settings.IPAddress, settings.Port));
+
+					consulClient.RegisterService (serviceId, endPoint, testEndPoint);
+					//consulClient.DeRegisterService ("");
+
+				} catch (Exception ex) {
+					while (ex.InnerException != null) {
+						ex = ex.InnerException;
+					}
+					Console.WriteLine ("Consul Registration Failed [{0}].", ex.Message);
+				}
+
+				//get fitness function from a dll
+				if (string.IsNullOrWhiteSpace (settings.FitnessAssemblyName)) {
+					throw new ApplicationException ("The Fitness assembly name must be specified using the -f argument.");
+				}
+
+				//nice welcome message
+				Console.WriteLine ("GAF Evaluation Server Listening on {0}:{1}.",
+					settings.IPAddress,
+					settings.Port);
+
+				var evaluationServer = new GAF.Network.EvaluationServer ();
+				evaluationServer.OnEvaluationComplete += OnEvaluationComplete;
+
+				//start the server
+				evaluationServer.Start (settings.IPAddress, settings.Port);
+
+
+			} catch (Exception ex) {
+
+				while (ex.InnerException != null) {
+					ex = ex.InnerException;
+				}
+				Console.WriteLine (ex.Message);
+			}
+
+			return 0;
+		}
+
+		public static void OnEvaluationComplete (object sender, GAF.Network.RemoteEvaluationEventArgs e)
+		{
+			//this event fires each time an evaluation is undertaken by the server
+			Console.WriteLine ("Evaluated solution {0}, Fitness={1}", e.Solution.Id, e.Solution.Fitness);
+		}
+
+	}
+}
