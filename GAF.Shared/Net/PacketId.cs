85379751aa0f1e0ff9ece66d0fb123d9d6038d27
John Newcombe <john.newcombe@me.com>
2016-08-13 22:14:48 +0100

    Updated PacketID enum.

--- GAF.Shared/Net/PacketId.cs
+++ GAF.Shared/Net/PacketId.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 namespace GAF.Net
 {
+    /// <summary>
+    /// Packet identifier.
+    /// </summary>
 	[Obsolete ("This enumerator is deprecated. The similarly named enumerator within the GAF.Network package, should be used instead.")]
 	public enum PacketId
 	{
+        /// <summary>
+        /// Data PID.
+        /// </summary>
 		Data,
-		Init,
-		Etx
+		/// <summary>
+        /// Init PID.
+        /// </summary>
+        Init,
+		/// <summary>
+        /// Etx PID.
+        /// </summary>
+        Etx
 	}
 }
 
