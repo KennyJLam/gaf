68e980ffba79187e516b8006c2b24210284ec48e
John Newcombe <john.newcombe@me.com>
2016-09-10 13:38:31 +0100

    Added Terminate method to interface due to move to IFitness.

--- GAF.Api/IGafLabConsumerFunctions.cs
+++ GAF.Api/IGafLabConsumerFunctions.cs
@@ -2,8 +2,16 @@
 
 namespace GAF.Api
 {
-	public interface IGafLabConsumerFunctions : IConsumerFunctions
+	public interface IGafLabConsumerFunctions : IFitness
 	{
+		/// <summary>
+		/// This method should implement the GA's terminate function.
+		/// </summary>/// <returns><c>true</c>, if algorithm was terminated, <c>false</c> otherwise.</returns>
+		/// <param name="population">Population.</param>
+		/// <param name="currentGeneration">Current generation.</param>
+		/// <param name="currentEvaluation">Current evaluation.</param>
+		bool TerminateAlgorithm (GAF.Population population, int currentGeneration, long currentEvaluation);
+
 		/// <summary>
 		/// This interface is provided to support the GAF.Lab GUI application. Please see the product information
 		/// for GAF.Lab for further details.
