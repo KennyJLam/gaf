ac4f187b928f5fe0a1e0c2a05e0bf954036485d8
John Newcombe <john.newcombe@me.com>
2016-09-10 13:37:20 +0100

    Added GAF Lab Consumer Functions example projects.

---
+++ Example.IGafLabConsumerFunctions.BinaryF6/BinaryF6Functions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using GAF;
+
+namespace Example.IGafLabConsumerFunctions.BinaryF6
+{
+	public class BinaryF6Functions : GAF.Api.IGafLabConsumerFunctions
+	{
+		#region IGafLabConsumerFunctions implementation
+
+		public double EvaluateFitness (GAF.Chromosome chromosome)
+		{
+			double fitnessValue = -1;
+			if (chromosome != null) {
+				//this is a range constant that is used to keep the x/y range between -100 and +100
+				var rangeConst = 200 / (System.Math.Pow (2, chromosome.Count / 2) - 1);
+
+				//get x and y from the solution
+				var x1 = Convert.ToInt32 (chromosome.ToBinaryString (0, chromosome.Count / 2), 2);
+				var y1 = Convert.ToInt32 (chromosome.ToBinaryString (chromosome.Count / 2, chromosome.Count / 2), 2);
+
+				//Adjust range to -100 to +100
+				var x = (x1 * rangeConst) - 100;
+				var y = (y1 * rangeConst) - 100;
+
+				//using binary F6 for fitness.
+				var temp1 = System.Math.Sin (System.Math.Sqrt (x * x + y * y));
+				var temp2 = 1 + 0.001 * (x * x + y * y);
+				var result = 0.5 + (temp1 * temp1 - 0.5) / (temp2 * temp2);
+
+				fitnessValue = 1 - result;
+			} else {
+				//chromosome is null
+				throw new ArgumentNullException ("chromosome", "The specified Chromosome is null.");
+			}
+
+			return fitnessValue;
+		}
+
+		public bool TerminateAlgorithm (Population population, int currentGeneration, long currentEvaluation)
+		{
+			return currentGeneration >= 350;
+		}
+
+		public Population CreatePopulation ()
+		{
+			//standard random binary chromosome (44bits) with a population of 100
+			var population = new Population (100, 44, false, false, ParentSelectionMethod.StochasticUniversalSampling, true);
+			return population;
+		}
+
+		public string CreateGenerationCompleteMessage (Population currentPopulation, int currentGeneration, long evaluations)
+		{
+			var message = string.Format ("Generation: {0}, Evaluations: {1}", 
+				              currentGeneration,
+				              evaluations);
+			return message;
+		}
+
+		public string CreateRunCompleteMessage (Population currentPopulation, int currentGeneration, long evaluations)
+		{
+			var message = string.Format ("Generation: {0}, Evaluations: {1}", 
+				currentGeneration,
+				evaluations);
+			return message;
+		}
+
+		#endregion
+	}
+}
+
+
---
+++ Example.IGafLabConsumerFunctions.TravellingSalesman/City.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.IGafLabConsumerFunctions.TravellingSalesman
+{
+	[Serializable]
+    public class City
+    {
+        public City(string name, double latitude, double longitude)
+        {
+            Name = name;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public string Name { set; get; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        /// <summary> 
+        /// Returns the distance in Km between this City and the specified location. 
+        ///<summary> 
+        public double GetDistanceFromPosition(double latitude, double longitude)
+        {
+            var R = 6371;
+            // radius of the earth in km 
+            var dLat = DegreesToRadians(latitude - Latitude);
+            var dLon = DegreesToRadians(longitude - Longitude);
+            var a = System.Math.Sin(dLat/2)*System.Math.Sin(dLat/2) +
+                    System.Math.Cos(DegreesToRadians(Latitude))*System.Math.Cos(DegreesToRadians(latitude))*
+                    System.Math.Sin(dLon/2)*System.Math.Sin(dLon/2);
+            var c = 2*System.Math.Atan2(System.Math.Sqrt(a), System.Math.Sqrt(1 - a));
+            var d = R*c;
+            // distance in km 
+            return d;
+        }
+
+        private static double DegreesToRadians(double deg)
+        {
+            return deg*(System.Math.PI/180);
+        }
+
+		public override string ToString ()
+		{
+			return Name;
+		}
+    }
+}
